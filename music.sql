6877f3c24371360db878ff430ef960ffaeaefb49
Omaima <omaimasharhan@gmail.com>
2023-11-12 15:32:53 -0500

    UPDATE

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
 
+CREATE TABLE Artists (
+    artist_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Albums (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    artist_id INT,
+    FOREIGN KEY (artist_id) REFERENCES Artists(artist_id)
+);
+
+CREATE TABLE Songs (
+    song_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    album_id INT,
+    track_number INT NOT NULL,
+    duration_seconds INT NOT NULL,
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id)
+);
+
